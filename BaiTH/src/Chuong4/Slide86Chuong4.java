c6678aa2ffb5cfac247d9d5ae8413fbe9e9d2684
Tanhhhhhhhh <tuananh13052004@gmail.com>
2024-05-17 09:07:04 +0700

    Slide86Chuong4

---
+++ BaiTH/src/Chuong4/Slide86Chuong4.java
@@ -0,0 +1,18 @@
+package Chuong4;
+
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Map;
+
+public class Slide86Chuong4 {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("CSLT", "Co so lap trinh ");
+        hashMap.put("C++", "C++");
+        hashMap.put("PHP", "PHP");
+        hashMap.put("Java", "Java");
+        Set<Map.Entry <String, String>> setHashMap = hashMap.entrySet();
+        System.out.println("Cac entry co trong setHashMap: ");
+        System.out.println(setHashMap);
+    }
+}
